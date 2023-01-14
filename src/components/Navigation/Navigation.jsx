f5d2ce3767579fc58da53887c522e278ec914766
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-01-14 21:55:31 +0300

    Fixed nav according to token

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Nav from 'components/Nav/Nav';
 import UserNav from 'components/UserNav/UserNav';
 import AuthNav from 'components/AuthNav/AuthNav';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { getToken } from 'redux/Auth/auth-selectors';
 
 export default function Navigation() {
-  // const token = useSelector();
+  const token = useSelector(getToken);
 
   return (
     <div>
       <Nav />
-      <UserNav />
-      <AuthNav />
-      {/* {token ? <UserNav /> : <AuthNav />} */}
+      {token ? <UserNav /> : <AuthNav />}
     </div>
   );
 }
