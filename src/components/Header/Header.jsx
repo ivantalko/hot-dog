9468b499b273bef3b8e05e416caa1ac1e0a7ad98
OlesRezvaniuk <orezvaniuk@gmail.com>
2023-01-14 14:18:50 +0200

    к
    к

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const NavLinkStyled = styled(NavLink)`
-  &:active {
-    text-decoration: none;
-    color: red;
-  }
-`;
-
 export const Header = () => {
   return (
     <>
