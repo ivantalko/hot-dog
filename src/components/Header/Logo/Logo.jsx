22ce7dbf412eb44b7cbb97b653ea4718ecc25104
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-01-18 19:13:08 +0300

    Made Logo - Link

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -5,23 +5,26 @@ import logoTab1 from '../../../helpers/images/logo/petlylogo-tab-1x.png';
 import logoTab2 from '../../../helpers/images/logo/petlylogo-tab-2x.png';
 import logoMob1 from '../../../helpers/images/logo/petlylogo-mob-1x.png';
 import logoMob2 from '../../../helpers/images/logo/petlylogo-mob-2x.png';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <picture>
-      <source
-        srcSet={`${logoComp1} 1x, ${logoComp2} 2x`}
-        media="(min-width: 1280px)"
-      />
-      <source
-        srcSet={`${logoTab1} 1x, ${logoTab2} 2x`}
-        media="(min-width: 768px)"
-      />
-      <source
-        srcSet={`${logoMob1} 1x, ${logoMob2} 2x`}
-        media="(max-width: 767px)"
-      />
-      <img src={logoComp1} alt="logo" />
-    </picture>
+    <Link to="/">
+      <picture>
+        <source
+          srcSet={`${logoComp1} 1x, ${logoComp2} 2x`}
+          media="(min-width: 1280px)"
+        />
+        <source
+          srcSet={`${logoTab1} 1x, ${logoTab2} 2x`}
+          media="(min-width: 768px)"
+        />
+        <source
+          srcSet={`${logoMob1} 1x, ${logoMob2} 2x`}
+          media="(max-width: 767px)"
+        />
+        <img src={logoComp1} alt="logo" />
+      </picture>
+    </Link>
   );
 }
