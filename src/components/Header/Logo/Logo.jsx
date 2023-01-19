6e3a838d3b8b27d17394e74c161791c5949ac542
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-01-19 14:09:50 +0300

    Fixed Logo Link

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -6,10 +6,14 @@ import logoTab2 from '../../../helpers/images/logo/petlylogo-tab-2x.png';
 import logoMob1 from '../../../helpers/images/logo/petlylogo-mob-1x.png';
 import logoMob2 from '../../../helpers/images/logo/petlylogo-mob-2x.png';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getToken } from 'redux/Auth/auth-selectors';
 
 export default function Logo() {
+  const token = useSelector(getToken);
+
   return (
-    <Link to="/">
+    <Link to={token ? '/notices/sell' : '/'}>
       <picture>
         <source
           srcSet={`${logoComp1} 1x, ${logoComp2} 2x`}
