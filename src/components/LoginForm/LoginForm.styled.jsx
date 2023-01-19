377d3e1a09bdf0ba97831fe29d0831da634a7e76
Valeriia tymchenko <lera.tymchenko@ukr.net>
2023-01-19 12:57:03 +0200

    summary(required)

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -7,13 +7,14 @@ export const FormList = styled.div`
     background: #ffffff;
     box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
     border-radius: 40px;
-
     width: 608px;
+    height: 449px;
     padding: 60px 80px 40px 80px;
   }
 
   @media ${breakpoints.desktop} {
     width: 618px;
+    height: 473px;
     padding: 60px 80px;
   }
 `;
