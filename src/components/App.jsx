26e07b3a132062ef3571961df01190521876c025
ivantalko <108145743+ivantalko@users.noreply.github.com>
2023-01-22 16:47:21 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import UserPage from 'pages/UserPage/UserPage';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import OurFriends from '../pages/OurFriends/OurFriends'
+import OurFriends from '../pages/OurFriends/OurFriends';
 import RegisterPage from 'pages/RegisterPage';
 import LoginPage from 'pages/LoginPage';
 import { NoticiesCategoriesList } from './NoticesCategoriesList/NoticesCategoriesList';
@@ -58,11 +58,7 @@ export const App = () => {
           />
         </Route>
 
-        <Route path="friends" element={<OurFriends/>} />
-        <Route path="login" element={<>login Page</>} />
-        <Route path="register" element={<>register Page</>} />
-        <Route path="user" element={<>user Page</>} />
-        <Route path="friends" element={<>friends Page</>} />
+        <Route path="friends" element={<OurFriends />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="user" element={<UserPage />} />
