21074693a5e1f38e67d464f7db6b0165a16d0186
Viktoriia <viktoria.murzhak@gmail.com>
2023-01-22 18:59:17 +0200

    sort news

--- src/components/News/NewsList/NewsList.jsx
+++ src/components/News/NewsList/NewsList.jsx
@@ -2,13 +2,19 @@ import { NewsItem } from '../NewsItem/NewsItem';
 import { ErrorSearch, List } from './NewsList.styled';
 
 export const NewsList = ({ news, isFiltered, filteredNews }) => {
+  // const normDate = date => {
+  //   return Date.parse(date);
+  // };
+
+  // const sortedNews = news.sort((a, b) => normDate(b.date) - normDate(a.date));
+
   return (
     <>
       {!isFiltered || filteredNews.length > 0 ? (
         <List>
           {!isFiltered
             ? news
-                ?.slice(0, 6)
+                .slice(0, 6)
                 .map(item => (
                   <NewsItem
                     key={item.title}
