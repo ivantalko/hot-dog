85ed3d120e19dd6b9e5f4685b9b8d2fbad8d0b02
Makievskiy Dmytro <dimamakievskii@gmail.com>
2023-01-24 15:52:45 +0200

    fix finally

--- src/components/User/UserCard/UserInfo/UserInfo.jsx
+++ src/components/User/UserCard/UserInfo/UserInfo.jsx
@@ -1,5 +1,5 @@
 // import axios from 'axios';
-// import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
   selectBirthday,
